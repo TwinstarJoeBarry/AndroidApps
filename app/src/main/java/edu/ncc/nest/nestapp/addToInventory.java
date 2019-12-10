bc1c3daf39711a66d6f4c5ffccdf4e9f7ea96c0d
Sixto Interiano <six.interiano@gmail.com>
2019-12-10 17:18:33 -0500

    adding but fixing

--- app/src/main/java/edu/ncc/nest/nestapp/addToInventory.java
+++ app/src/main/java/edu/ncc/nest/nestapp/addToInventory.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 
@@ -26,9 +27,12 @@ import android.widget.Toast;
 public class addToInventory extends AppCompatActivity {
 
     ListView listView;
-    String mcategory[] = {"Baby Food", "Baked Goods", "Beverages", "Condiments, Sauces & Canned Goods"};
-    String msubExamples[] = {"Cereal, Dinners, Formula, etc.", "Baking, Refrigerated Dough, etc", "Sodas, Juices, Wine, Coffee, etc.", "BBQ, Ketchup, Broth, Cans, etc."};
-    int images[] = {R.drawable.baby_food, R.drawable.baked_goods, R.drawable.beverages, R.drawable.condiments_sauce_and_can_goods};
+
+    String[] mCategory = {"Baby Food", "Baked Goods", "Beverages", "Condiments, Sauces & Canned Goods", "Dairy Products & Eggs", "Deli & Prepared Foods", "Food Purchased Frozen", "Grains, Beans & Pasta", "Meat", "Poultry", "Produce", "Seafood", "Shelf Stable Food", "Vegetarian Proteins"};
+
+    String[] mSubExamples = {"Cereal, Dinners, Formula, etc.", "Baking, Refrigerated Dough, etc", "Sodas, Juices, Wine, Coffee, etc.", "BBQ, Ketchup, Broth, Cans, etc.", "Butter, Milk, Cheese, etc.", "Prepackaged Meat, Left Overs, Salads, PreCooked Foods, etc.", "Frozen Pizza, Frozen Stuff, Ice cream, etc.", "Dried Vegetables, Grains, Whole Foods, etc.", "Fresh Meat, Smoked, Stuffed, etc.", "Chicken, Birds, Something That Can Flap It's Wings, etc.", "Fruits, Vegetables, etc.", "Fresh Fish, Shellfish, Smoked, etc.", "Chocolate Syrup, oils, canned fruits, etc.", "Miso, Tofu, Soy..., etc."};
+
+    int[] images = {R.drawable.baby_food, R.drawable.baked_goods, R.drawable.beverages, R.drawable.condiments_sauce_and_can_goods, R.drawable.dairy_products, R.drawable.deli_products, R.drawable.frozen_foods, R.drawable.grains_beans_pasta, R.drawable.meats, R.drawable.poultry, R.drawable.product_grocery, R.drawable.fishes, R.drawable.shelf, R.drawable.vegetales};
 
 
     @Override
@@ -41,10 +45,11 @@ public class addToInventory extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Adaptor newAdaptor = new Adaptor(this, mcategory, msubExamples, images);
+        Adaptor newAdaptor = new Adaptor(this, mCategory, mSubExamples, images);
         listView.setAdapter(newAdaptor);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if(position == 0)
@@ -63,6 +68,46 @@ public class addToInventory extends AppCompatActivity {
                 {
                     Toast.makeText(addToInventory.this, "Condiments Test", Toast.LENGTH_SHORT).show();
                 }
+                if(position == 4)
+                {
+                    Toast.makeText(addToInventory.this, "Dairy Test", Toast.LENGTH_SHORT).show();
+                }
+                if(position == 5)
+                {
+                    Toast.makeText(addToInventory.this, "Deli Test", Toast.LENGTH_SHORT).show();
+                }
+                if(position == 6)
+                {
+                    Toast.makeText(addToInventory.this, "Food Purchased Frozen Test", Toast.LENGTH_SHORT).show();
+                }
+                if(position == 7)
+                {
+                    Toast.makeText(addToInventory.this, "Grains Test", Toast.LENGTH_SHORT).show();
+                }
+                if(position == 8)
+                {
+                    Toast.makeText(addToInventory.this, "Meat Test", Toast.LENGTH_SHORT).show();
+                }
+                if(position == 9)
+                {
+                    Toast.makeText(addToInventory.this, "Poultry Test", Toast.LENGTH_SHORT).show();
+                }
+                if(position == 10)
+                {
+                    Toast.makeText(addToInventory.this, "Produce Test", Toast.LENGTH_SHORT).show();
+                }
+                if(position == 11)
+                {
+                    Toast.makeText(addToInventory.this, "Sea food test", Toast.LENGTH_SHORT).show();
+                }
+                if(position == 12)
+                {
+                    Toast.makeText(addToInventory.this, "shelf food", Toast.LENGTH_SHORT).show();
+                }
+                if(position == 13)
+                {
+                    Toast.makeText(addToInventory.this, "vegetales Test", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -71,11 +116,11 @@ public class addToInventory extends AppCompatActivity {
     class Adaptor extends ArrayAdapter<String>
     {
         Context context;
-        String rCategory[];
-        String rSubExamples[];
-        int rImg[];
+        String[] rCategory;
+        String[] rSubExamples;
+        int[] rImg;
 
-        Adaptor (Context c, String category[], String subExamples[], int imgs[])
+        Adaptor (Context c, String[] category, String[] subExamples, int[] imgs)
         {
             super(c,R.layout.rows, R.id.mainText, category);
             this.context = c;
