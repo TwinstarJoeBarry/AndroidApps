900822a453221f29f32a3ed788be3b0ff00298ae
Akashdeep Singh <ziles715@gmail.com>
2020-04-25 22:23:36 +0000

    Updated NoN-Menu UI by adding categories of foods in Inventory that will launch new activities through intents

---
+++ app/src/main/java/edu/ncc/nest/nestapp/UIFoods/Condiments.java
@@ -0,0 +1,64 @@
+package edu.ncc.nest.nestapp.UIFoods;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import edu.ncc.nest.nestapp.Choose;
+import edu.ncc.nest.nestapp.R;
+
+public class Condiments extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[] btns;
+    @Override
+    /**
+     * onCreate method -
+     * loads the 'activity_Beverages layout' which has only a TextView as a placeholder for now
+     */
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condiments);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    //implements the menu options for the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.homeBtn) {
+            home();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * home method - goes to the home screen
+     */
+    public void home() {
+        Intent intent = new Intent(this, Choose.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
+
