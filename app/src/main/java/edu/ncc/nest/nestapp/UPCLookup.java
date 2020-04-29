6d63441a0be69bce6bcae2029c49dc8ddc004887
Matt Vitelli <mvboy39@gmail.com>
2020-04-29 18:53:27 -0400

    got started, lots of work left to do

--- app/src/main/java/edu/ncc/nest/nestapp/UPCLookup.java
+++ app/src/main/java/edu/ncc/nest/nestapp/UPCLookup.java
@@ -2,13 +2,21 @@ package edu.ncc.nest.nestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class UPCLookup extends AppCompatActivity {
+    private SQLiteDatabase fdcid_to_upc_database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u_p_c_lookup);
+
+        this.fdcid_to_upc_database = SQLiteDatabase.openDatabase(this.getResources().getResourceEntryName(R.raw.fdcid_to_upc), null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    public int getFDCID(long upc) {
+        return 0; //TODO
     }
 }
