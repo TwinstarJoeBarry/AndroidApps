2a4a6b2d29681e0c13362c6a93552874ecf31775
lpostner <Lori.Postner@ncc.edu>
2020-11-14 15:42:51 -0500

    Copied the scanner code from Scanner.java to ScanFragment.java and updated to work from within a fragment

--- app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/ScanFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/ScanFragment.java
@@ -1,18 +1,47 @@
 package edu.ncc.nest.nestapp.FragmentsUpc;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import edu.ncc.nest.nestapp.FoodItem;
 import edu.ncc.nest.nestapp.R;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class ScanFragment extends Fragment {
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.util.Scanner;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanFragment extends Fragment implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
+    private TextView txtResult;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -25,6 +54,9 @@ public class ScanFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        scannerView = (ZXingScannerView) getView().findViewById(R.id.zxscan);
+        txtResult = (TextView) getView().findViewById(R.id.txt_result);
+
         view.findViewById(R.id.button_scan_complete).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,5 +64,45 @@ public class ScanFragment extends Fragment {
                         .navigate(R.id.action_ScanFragment_to_StartFragment);
             }
         });
+
+        Dexter.withActivity(getActivity())
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        scannerView.setResultHandler(ScanFragment.this);
+                        scannerView.startCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                        Toast.makeText(getActivity(),
+                                "you must accept this permission",
+                                Toast.LENGTH_LONG).show();
+
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                })
+                .check();
+    }
+    @Override
+    public void onDestroy(){
+        scannerView.stopCamera();
+        super.onDestroy();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        //here we can recieve rawResult
+        txtResult.setText(rawResult.getText());
+        scannerView.startCamera();
+        String str = rawResult.getText();
+
     }
 }
