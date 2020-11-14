497c34540c696039cb3e0e733bf75ab1d7814de0
lpostner <Lori.Postner@ncc.edu>
2020-11-14 16:06:42 -0500

    Added the fragment for entering the UPC manually

--- app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/EnterUpcFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/EnterUpcFragment.java
@@ -1,17 +1,18 @@
 package edu.ncc.nest.nestapp.FragmentsUpc;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import edu.ncc.nest.nestapp.R;
 
-public class StartFragment extends Fragment {
+public class EnterUpcFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -19,25 +20,19 @@ public class StartFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        return inflater.inflate(R.layout.fragment_enter_upc, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_scan).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_enter_next).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(StartFragment.this)
+                /*
+                NavHostFragment.findNavController(EnterUpcFragment.this)
                         .navigate(R.id.action_StartFragment_to_ScanFragment);
-            }
-        });
-
-        view.findViewById(R.id.button_enter).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(StartFragment.this)
-                        .navigate(R.id.action_StartFragment_to_EnterUpcFragment);
+                 */
             }
         });
     }
