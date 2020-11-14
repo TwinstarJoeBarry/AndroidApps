7c3ed7ce44ca7c5f5b32d8d989c0602e85f79edf
lpostner <Lori.Postner@ncc.edu>
2020-11-14 14:24:18 -0500

    Created the initial two fragments for the Check Expiration button

---
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/ScanFragment.java
@@ -0,0 +1,36 @@
+package edu.ncc.nest.nestapp.FragmentsUpc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import edu.ncc.nest.nestapp.R;
+
+public class ScanFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scan, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ScanFragment.this)
+                        .navigate(R.id.action_ScanFragment_to_ConfirmItemFragment);
+            }
+        });
+    }
+}
