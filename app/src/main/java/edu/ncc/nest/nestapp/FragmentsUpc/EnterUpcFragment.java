426eb02aa6ec81fef2b118fd4d12ebec1446fad4
lpostner <Lori.Postner@ncc.edu>
2020-11-14 16:24:04 -0500

    Added licenses to fragment xml and java files

--- app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/EnterUpcFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/EnterUpcFragment.java
@@ -1,10 +1,25 @@
 package edu.ncc.nest.nestapp.FragmentsUpc;
-
+/**
+ *
+ * Copyright (C) 2020 The LibreFoodPantry Developers.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
