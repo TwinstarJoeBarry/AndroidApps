6c6e462081cf8a4061daab460ed217bfc08b9986
Tyler Sizse <ANewGalaxy@hotmail.com>
2020-11-20 19:02:53 -0500

    Added a empty FragmentActivity called GuestVisit.java with a new layout file called activity_guest_visit.xml that contains the fragment navgraph. They will be used for navigation of the fragments that GuestScanFragment.java will navigate to.

---
+++ app/src/main/java/edu/ncc/nest/nestapp/GuestVisit.java
@@ -0,0 +1,39 @@
+package edu.ncc.nest.nestapp;
+/**
+ *
+ * Copyright (C) 2019 The LibreFoodPantry Developers.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * GuestVisit - Activity to use for navigation between fragments when a guest checks-in
+ */
+public class GuestVisit extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_visit);
+
+
+
+    }
+
+}
