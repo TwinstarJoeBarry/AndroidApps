769da7f675ab969cb13198fef5c065cfa73153fe
Tyler Sizse <ANewGalaxy@hotmail.com>
2020-11-22 23:25:00 -0500

    Reorganized some code for better readability

--- app/src/main/java/edu/ncc/nest/nestapp/GuestFormHelper.java
+++ app/src/main/java/edu/ncc/nest/nestapp/GuestFormHelper.java
@@ -19,6 +19,7 @@ package edu.ncc.nest.nestapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
@@ -55,6 +56,29 @@ public class GuestFormHelper extends SQLiteOpenHelper {
 
     public static final String LOCATION = "location";
 
+    // database version and name
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "GuestFormInformation.db";
+
+    public GuestFormHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (" + _ID +
+                " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME +
+                " TEXT, " + EMAIL + " TEXT, " + DATE + "TEXT, " + ADDRESS + "TEXT, " +
+                CITY + "TEXT, " + ZIP + "TEXT, " + STATE + "TEXT, " + ADDITIONALINFO + "TEXT, " + NAMEOFVOLUNTEER + "TEXT, " +
+                NCCID + "TEXT, " + PHONE + " TEXT, " + BARCODE + " TEXT);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
     public boolean insertData(String name, String email, String phone, String date, String address, String city, String zip, String barcode) {
 
         //creating an SQLLite database
@@ -86,30 +110,5 @@ public class GuestFormHelper extends SQLiteOpenHelper {
         return true;
 
     }
-
-
-    // database version and name
-    private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "GuestFormInformation.db";
-
-    public GuestFormHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (" + _ID +
-                " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME +
-                " TEXT, " + EMAIL + " TEXT, " + DATE + "TEXT, " + ADDRESS + "TEXT, " +
-                CITY + "TEXT, " + ZIP + "TEXT, " + STATE + "TEXT, " + ADDITIONALINFO + "TEXT, " + NAMEOFVOLUNTEER + "TEXT, " +
-                NCCID + "TEXT, " + PHONE + " TEXT, " + BARCODE + " TEXT);");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
-    }
-
 
 }
