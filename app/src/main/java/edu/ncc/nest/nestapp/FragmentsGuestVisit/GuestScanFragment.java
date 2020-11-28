5c70db92bea35d0f8916a1512fc4cf996e02142e
Tyler Sizse <ANewGalaxy@hotmail.com>
2020-11-28 08:17:40 -0500

    Changed GuestScanFragment.java layout to fragment_guest_scan.xml

--- app/src/main/java/edu/ncc/nest/nestapp/FragmentsGuestVisit/GuestScanFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentsGuestVisit/GuestScanFragment.java
@@ -293,7 +293,7 @@ public class GuestScanFragment extends Fragment implements View.OnClickListener 
             Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false);
+        return inflater.inflate(R.layout.fragment_guest_scan, container, false);
 
     }
 
