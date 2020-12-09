623065409f0e4d53905d371a533a7133f2154d00
Alex Gibbons <alexaderg@gmail.com>
2020-12-09 12:23:12 -0500

    Forgot to add files to previous commit.

--- app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/ConfirmItemFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/ConfirmItemFragment.java
@@ -20,15 +20,33 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.fragment.NavHostFragment;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import edu.ncc.nest.nestapp.NestUPC;
 import edu.ncc.nest.nestapp.R;
 
 public class ConfirmItemFragment extends Fragment {
+
+    private Button confirmButton;
+    private Button incorrectButton;
+    private TextView itemName;
+    private TextView catName;
+    private TextView upc;
+
+    private String item_name;
+    private String cat_name;
+    private String upc_string;
+
+    private NestUPC item;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -39,36 +57,51 @@ public class ConfirmItemFragment extends Fragment {
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+            super.onViewCreated(view, savedInstanceState);
 
 
+            // Get respective views from layout
+            confirmButton = (Button) view.findViewById(R.id.button_confirm_item);
+            incorrectButton = (Button) view.findViewById(R.id.button_incorrect_item);
+            itemName = (TextView) view.findViewById(R.id.add_item_name);
+            catName = (TextView) view.findViewById(R.id.add_cat_name);
+            upc = (TextView) view.findViewById(R.id.add_UPC);
 
-        // if confirm button clicked, nav to fragment_select_printed_expiration_date.xml
-        view.findViewById(R.id.button_confirm_next).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            // Retrieve Strings from data
+            Bundle bundle = new Bundle();
+            item = (NestUPC) bundle.get("GET FOOD ITEM");
+            item_name = item.getProductName();
+            cat_name = item.getCatDesc();
+            upc_string = item.getUpc();
 
-                NavHostFragment.findNavController(ConfirmItemFragment.this)
-                        .navigate(R.id.action_confirmItemFragment_to_selectPrintedExpirationDateFragment);
+            // Sed retrieved data to TextView
+            itemName.setText(item_name);
+            catName.setText(cat_name);
+            upc.setText(upc_string);
 
-            }
-        });
+            ////////////// Navigation //////////////
 
-        // if the item was incorrect, nav to fragment_select_item.xml
-        view.findViewById(R.id.button_not_correct).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            // if confirm button clicked, nav to fragment_select_printed_expiration_date.xml
+            view.findViewById(R.id.button_confirm_item).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
-                NavHostFragment.findNavController(ConfirmItemFragment.this)
-                        .navigate(R.id.action_confirmItemFragment_to_selectItemFragment);
+                    NavHostFragment.findNavController(ConfirmItemFragment.this)
+                            .navigate(R.id.action_confirmItemFragment_to_selectPrintedExpirationDateFragment);
 
-            }
-        });
+                }
+            });
+
+            // if the item was incorrect, nav to fragment_select_item.xml
+            view.findViewById(R.id.button_incorrect_item).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    NavHostFragment.findNavController(ConfirmItemFragment.this)
+                            .navigate(R.id.action_confirmItemFragment_to_selectItemFragment);
+
+                }
+            });
 
     }
-
-
-
-
-
 }
