3d5a431bfb68ff14018113fe755794f8b74761e3
Pete Lawson <N00661386@students.ncc.edu>
2020-12-09 16:09:18 -0500

    select_item_fragment issue 109 porting code from NestNewUPC / ItemInformation / activity_item_info / activity_nest_new_upc

--- app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/SelectItemFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/SelectItemFragment.java
@@ -16,38 +16,174 @@ package edu.ncc.nest.nestapp.FragmentsUpc;
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import edu.ncc.nest.nestapp.NestDBDataSource;
 import edu.ncc.nest.nestapp.R;
 
-public class SelectItemFragment extends Fragment {
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        // Inflate the layout for this fragment
+public class SelectItemFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener
+{
+    NestDBDataSource database;
+    String name, description;
+    int category, product;
+
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         return inflater.inflate(R.layout.fragment_select_item, container, false);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
+    {
+
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_select_next).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // navigation has yet to be set up in the nav_graph.xml
-                NavHostFragment.findNavController(SelectItemFragment.this)
-                        .navigate(R.id.action_StartFragment_to_ScanFragment);
+        /** CATEGORY SPINNER - SET UP ARRAY ADAPTER LIST */
+        Spinner categorySpinner = (Spinner)view.findViewById(R.id.categorySpinner);
+
+        ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource( getContext(),
+                R.array.categories_array, android.R.layout.simple_spinner_item );
+
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(categoryAdapter);
+
+        categorySpinner.setOnItemSelectedListener(this);
+
+
+        /** ACCEPT BUTTON CODE - PARSE AND ADD VALES TO NEW UPC, PASS INFO TO PRINTED EXPIRATION DATE */
+        view.findViewById(R.id.acceptButton).setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View view)
+            {
+                // open the database to parse the information
+                database = new NestDBDataSource(getContext());
+                String upcBeingAdded = "0123456789AB"; // TODO need to get this from scan/enter upc frag through bundle savedInstance
+
+                // retrieve the item information from each view, casting as appropriate
+                name = ((EditText) (getView().findViewById(R.id.brandEdit))).getText().toString();
+                description = ((EditText) (getView().findViewById(R.id.descriptionEdit))).getText().toString();
+
+//                Spinner categorySpinner = ((Spinner) (getView().findViewById(R.id.categorySpinner)));
+//                Spinner productSpinner = ((Spinner) (getView().findViewById(R.id.productSpinner)));
+//                int selectedCategoryId = 0, selectedProductId = 0; // need top ull these from array adapter
+
+
+                // make sure all fields were was available and entered properly
+                if (name.isEmpty() || description.isEmpty() )
+                    Toast.makeText(getContext(), "Please fill or select all fields!", Toast.LENGTH_LONG).show();
+
+                else
+                {
+                    // use dataSource to add the new upc to the Nest UPCs table
+                    Toast.makeText(getContext(), String.format("name: %s des: %s", name, description), Toast.LENGTH_LONG).show();
+//                    database.insertNewUPC(upcBeingAdded, name, description, product);
+
+
+                    // stuff everything in a bundle in case it's needed for PrintedExpirationDate
+                    Bundle saved = new Bundle();
+
+//                    String StringInfo[] = {name, description};
+//                    saved.putStringArray("savedStringInfo", StringInfo);
+//                    getParentFragmentManager().setFragmentResult("savedStringInfo", saved);
+                        // fragment result listener
+//                    int intInfo[] = {category, product};
+//                    saved.putIntArray("savedIntInfo", intInfo);
+//                    getParentFragmentManager().setFragmentResult("savedIntInfo", saved);
+
+                    NavHostFragment.findNavController(SelectItemFragment.this)
+                            .navigate(R.id.action_selectItemFragment_to_selectPrintedExpirationDateFragment);
+                }
             }
         });
+
+
+        /** CANCEL BUTTON CODE - NAVIGATE BACK TO ENTER UPC FRAG */
+        view.findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View view)
+            {
+                NavHostFragment.findNavController(SelectItemFragment.this)
+                        .navigate(R.id.confirmItemFragment);
+            }
+        });
+
+    } // onViewCreated()
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+
     }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
+    }
+
+
+    /**
+     * showItemMenu method --
+     * onClick for the Item button; loads and shows the item menu using the items ArrayList instance
+     * variable. Item menu is empty before a user selects a category.
+     *
+     * @param v - the Item button
+     */
+    public void showItemMenu(View v){
+        // nothing to show? skip it
+        // todo show a message if waiting?
+        // create popup menu for item selection
+        PopupMenu itemPop = new PopupMenu(getContext(), v);
+        // attach method to take action when item is selected by the user
+        itemPop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                // update selected item position and display
+                selectedItemPosition = item.getItemId();
+                itemDisplay.setText(item.getTitle().toString());
+                // todo do something regarding calculate button here (? - depends on implementation)
+                // todo make use of ShelfLife (may need to have user choose) of selected `Product` to calc date
+                return true;
+            }
+        });
+        // build the popup menu content by creating menuitems from the items list
+        for(int i = 0; i < items.size(); i++)
+            // Menu.NONE means this menuitem should not be part of any group
+            // i is our desired "Id" (position in the list, actually) for this menuitem
+            // i is also for the menuitem's display ordering position
+            // items.get(i). is the "item (subitem)" description
+            itemPop.getMenu().add(Menu.NONE, i, i, items.get(i).getName());
+        // todo should add subtitle if applicable, but this will necessitate using something other than PopupMenu
+        // show the popup menu
+        itemPop.show();
+    }
+
+
+
 }
