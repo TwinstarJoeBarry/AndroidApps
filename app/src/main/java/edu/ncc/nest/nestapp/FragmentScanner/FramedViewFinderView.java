74b76047ad66742325bfd350f202d81bc63e48ad
ANewGalaxy <ANewGalaxy@hotmail.com>
2020-12-14 06:04:41 -0500

    Updated the onDraw method to draw the frame

--- app/src/main/java/edu/ncc/nest/nestapp/FragmentScanner/FramedViewFinderView.java
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentScanner/FramedViewFinderView.java
@@ -53,7 +53,44 @@ public class FramedViewFinderView extends ViewfinderView {
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        if (frameStyle != 0 && framingRect != null) {
 
+            if (frameStyle == 2) {
+
+                int offset = (int) PAINT.getStrokeWidth() / 2;
+
+                int x1 = framingRect.left - frameOffset;
+                int y1 = framingRect.top - frameOffset;
+                int x2 = framingRect.right + frameOffset;
+                int y2 = framingRect.bottom + frameOffset;
+
+                int x1_offset = x1 - offset;
+                int y1_offset = y1 - offset;
+                int x2_offset = x2 + offset;
+                int y2_offset = y2 + offset;
+
+                // Top-Left Corner
+                canvas.drawLine(x1_offset, y1, x1_offset + frameCornerSize, y1, PAINT);
+                canvas.drawLine(x1, y1_offset, x1, y1_offset + frameCornerSize, PAINT);
+
+                // Top-Right Corner
+                canvas.drawLine(x2_offset - frameCornerSize, y1, x2_offset, y1, PAINT);
+                canvas.drawLine(x2, y1_offset, x2, y1_offset + frameCornerSize, PAINT);
+
+                // Bottom-Right Corner
+                canvas.drawLine(x2_offset, y2, x2_offset - frameCornerSize, y2, PAINT);
+                canvas.drawLine(x2, y2_offset - frameCornerSize, x2, y2_offset, PAINT);
+
+                // Bottom-Left Corner
+                canvas.drawLine(x1_offset, y2, x1_offset + frameCornerSize, y2, PAINT);
+                canvas.drawLine(x1, y2_offset - frameCornerSize, x1, y2_offset, PAINT);
+
+            } else
+
+                // Draw the frame as a rectangle
+                canvas.drawRect(framingRect, PAINT);
+
+        }
 
 
     }
