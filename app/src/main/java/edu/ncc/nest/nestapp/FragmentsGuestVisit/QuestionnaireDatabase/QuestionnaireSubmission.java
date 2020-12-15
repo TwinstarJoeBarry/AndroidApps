5241ccc258138e225403149c78a31629df15ed03
ANewGalaxy <ANewGalaxy@hotmail.com>
2020-12-15 10:42:03 -0500

    Created class variables and constructor for QuestionnaireSubmission.java

--- app/src/main/java/edu/ncc/nest/nestapp/FragmentsGuestVisit/QuestionnaireDatabase/QuestionnaireSubmission.java
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentsGuestVisit/QuestionnaireDatabase/QuestionnaireSubmission.java
@@ -1,4 +1,21 @@
 package edu.ncc.nest.nestapp.FragmentsGuestVisit.QuestionnaireDatabase;
 
+import java.util.ArrayList;
+
 public class QuestionnaireSubmission {
+
+    private final ArrayList<String> GUEST_ANSWERS;
+    public final String GUEST_ID;
+    public final long ROW_ID;
+
+    public QuestionnaireSubmission(long rowID, String guestID, ArrayList<String> guestAnswers) {
+
+        GUEST_ANSWERS = guestAnswers;
+
+        GUEST_ID = guestID;
+
+        ROW_ID = rowID;
+
+    }
+
 }
