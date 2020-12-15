536184f3febad0c9b63b9f24d491ac4fa5a25406
Alex Gibbons <alexaderg@gmail.com>
2020-12-15 11:21:50 -0500

    Attempting to implement DatePicker, does not work yet. Tried using code from stack overFlow. Will probably startover with my own code.

--- app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/SelectPrintedExpirationDateFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/FragmentsUpc/SelectPrintedExpirationDateFragment.java
@@ -24,21 +24,34 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import androidx.fragment.app.FragmentResultListener;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.ncc.nest.nestapp.DatePickerFragment;
+import edu.ncc.nest.nestapp.NestUPC;
 import edu.ncc.nest.nestapp.R;
 
 import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
-public class SelectPrintedExpirationDateFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+public class SelectPrintedExpirationDateFragment extends Fragment {
+
+    private static final String TAG = "TESTING";
 
     private String selectedDate;
+    private String expirationDate;
+    private NestUPC item;
+
+    private TextView date;
+
 
     @Override
     public View onCreateView(
@@ -52,6 +65,16 @@ public class SelectPrintedExpirationDateFragment extends Fragment implements Dat
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        date = view.findViewById(R.id.selected_date);
+
+        if (savedInstanceState == null) {
+            getParentFragmentManager().setFragmentResultListener("FOOD ITEM", this, new FragmentResultListener() {
+                @Override
+                public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                    item = (NestUPC) savedInstanceState.get("foodItem");
+                }
+            });
+        }
 
         // select date button pressed
         view.findViewById(R.id.button_select_date).setOnClickListener(new View.OnClickListener() {
@@ -59,15 +82,11 @@ public class SelectPrintedExpirationDateFragment extends Fragment implements Dat
             @Override
             public void onClick(View view) {
                 // Create a DatePicker object
-                DialogFragment datePicker = new DatePickerFragment();
+                DateDialogFragment datePicker = new DateDialogFragment();
                 // Show datePicker
                 datePicker.show(getParentFragmentManager(), "date picker");
-
-                
-
             }
         });
-
 
         view.findViewById(R.id.button_confirm_date).setOnClickListener(new View.OnClickListener() {
 
@@ -75,15 +94,43 @@ public class SelectPrintedExpirationDateFragment extends Fragment implements Dat
 
             @Override
             public void onClick(View view) {
+
+                // Send Date to selectItemFragment
+                Bundle dateBundle = new Bundle();
+                dateBundle.putString("DATE", expirationDate);
+
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("foodItem", item);
+
+                getParentFragmentManager().setFragmentResult("FOOD ITEM", dateBundle);
+
                 // navigation has yet to be set up in the nav_graph.xml
                 NavHostFragment.findNavController(SelectPrintedExpirationDateFragment.this)
-                        .navigate(R.id.action_StartFragment_to_ScanFragment);
+                        .navigate(R.id.displayTrueExpirationFragment);
             }
         });
     }
 
-    @Override
-    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+    public class DateDialogFragment extends DialogFragment  implements DatePickerDialog.OnDateSetListener{
 
+        public DateDialogFragment()
+        {
+        }
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            Calendar cal=Calendar.getInstance();
+            int year=cal.get(Calendar.YEAR);
+            int month=cal.get(Calendar.MONTH);
+            int day=cal.get(Calendar.DAY_OF_MONTH);
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            showSetDate(year,monthOfYear,dayOfMonth);
+        }
+
+    }
+
+    public void showSetDate(int year,int month,int day) {
+        date.setText(year+"/+"+month+"/"+day);
     }
 }
