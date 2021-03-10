aa79d7023d5b2172b9dc00e8eff5f7628511287a
Tyler Sizse <ANewGalaxy@hotmail.com>
2021-03-10 17:39:34 -0500

    Updated javadocs in OnProgressListener.java

--- app/src/main/java/edu/ncc/nest/nestapp/AsyncTask/OnProgressListener.java
+++ app/src/main/java/edu/ncc/nest/nestapp/AsyncTask/OnProgressListener.java
@@ -1,8 +1,19 @@
 package edu.ncc.nest.nestapp.AsyncTask;
 
+/**
+ * OnProgressListener --
+ * Mainly used to listen for any "progress" updates from a TaskExecutor.BackgroundTask object. Can
+ * also be used for other purposes.
+ * @param <Progress> The data type that will represent the "progress" of a task.
+ */
 @SuppressWarnings("unused")
 public interface OnProgressListener<Progress> {
 
+    /**
+     * onProgress --
+     * Called by a task, to inform the listener of any progress the related task has made.
+     * @param progress The "progress" of the related task.
+     */
     void onProgress(Progress progress);
 
 }
