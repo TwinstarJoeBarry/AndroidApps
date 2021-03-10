78628039b912e20b2f2f7a3b25d184ba821e7d74
Tyler Sizse <ANewGalaxy@hotmail.com>
2021-03-10 15:56:38 -0500

    Added additional lifecycle and action methods to BackgroundTask.java

--- app/src/main/java/edu/ncc/nest/nestapp/AsyncTask/BackgroundTask.java
+++ app/src/main/java/edu/ncc/nest/nestapp/AsyncTask/BackgroundTask.java
@@ -5,6 +5,8 @@ public abstract class BackgroundTask<Progress, Result> {
 
     private OnProgressListener<Progress> onProgressListener = null;
 
+    ///////////////////////////////// ON PROGRESS LISTENER METHODS /////////////////////////////////
+
     public final void setOnProgressListener(OnProgressListener<Progress> onProgressListener) {
 
         this.onProgressListener = onProgressListener;
@@ -14,6 +16,30 @@ public abstract class BackgroundTask<Progress, Result> {
     public final OnProgressListener<Progress> getOnProgressListener() {
 
         return onProgressListener;
+
+    }
+
+    ///////////////////////////////////// TASK ACTION METHODS //////////////////////////////////////
+
+    public final void postProgress(final Progress PROGRESS) {
+
+
+
+    }
+
+    //////////////////////////////////// TASK LIFECYCLE METHODS ////////////////////////////////////
+
+    protected void onPreExecute() {}
+
+    protected abstract Result doInBackground() throws Exception;
+
+    protected void onProgress(Progress progress) {}
+
+    protected void onPostExecute(Result result) {}
+
+    protected void onTaskFailed(Exception e) {
+
+        e.printStackTrace();
 
     }
 
