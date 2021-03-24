f031c0b503e45df3b99a551e43da033a70edb4b2
Tyler Sizse <ANewGalaxy@hotmail.com>
2021-03-24 18:49:34 -0400

    Updated a ScannerFragment to include the Scanner license

--- app/src/main/java/edu/ncc/nest/nestapp/GuestDatabaseRegistration/Fragments/ScannerFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/GuestDatabaseRegistration/Fragments/ScannerFragment.java
@@ -1,7 +1,6 @@
 package edu.ncc.nest.nestapp.GuestDatabaseRegistration.Fragments;
 
 /**
- *
  * Copyright (C) 2020 The LibreFoodPantry Developers.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,6 +15,20 @@ package edu.ncc.nest.nestapp.GuestDatabaseRegistration.Fragments;
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2012-2018 ZXing authors, Journey Mobile
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import android.os.Bundle;
