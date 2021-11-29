b0aa047147a97fb716b8542282ded410836d6074
Matt Brevetti <matt.soundli@gmail.com>
2021-11-29 17:19:37 -0500

    refactor. renamed fragment_third_form.xml to fragment_guest_database_reg... to follow naming convention. Updated references to the file automatically.

--- app/src/main/java/edu/ncc/nest/nestapp/GuestDatabaseRegistration/Fragments/ThirdFormFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/GuestDatabaseRegistration/Fragments/ThirdFormFragment.java
@@ -61,6 +61,6 @@ public class ThirdFormFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third_form, container, false);
+        return inflater.inflate(R.layout.fragment_guest_database_registration_third_form, container, false);
     }
 }
