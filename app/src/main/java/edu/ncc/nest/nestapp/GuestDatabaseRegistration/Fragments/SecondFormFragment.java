142ac65bd50c1cbf8033b2b1a1d185bf30418ad3
Richard Kim <RichardKim503@gmail.com>
2021-11-29 15:58:54 -0500

    Updating age array

--- app/src/main/java/edu/ncc/nest/nestapp/GuestDatabaseRegistration/Fragments/SecondFormFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/GuestDatabaseRegistration/Fragments/SecondFormFragment.java
@@ -44,7 +44,7 @@ import edu.ncc.nest.nestapp.databinding.FragmentGuestDatabaseRegistrationSecondF
  */
 public class SecondFormFragment extends Fragment {
 
-    //dfdfdfdfdfdfdfdfdfdfdf
+    //dfdfdfdfdfdfdfdfdfdf
     private FragmentGuestDatabaseRegistrationSecondFormBinding binding;
 
     private String inputStreetAddress1, inputStreetAddress2, inputCity, inputState, inputZip,
