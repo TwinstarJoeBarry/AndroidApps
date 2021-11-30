d1dc3c51b08c8161ab655eaf64ea264463e52073
wizng8 <wizng1@gmail.com>
2021-11-29 20:45:24 -0800

    Retrieving all necessary information from first&secondformfragment.

--- app/src/main/java/edu/ncc/nest/nestapp/GuestDatabaseRegistration/Fragments/SecondFormFragment.java
+++ app/src/main/java/edu/ncc/nest/nestapp/GuestDatabaseRegistration/Fragments/SecondFormFragment.java
@@ -53,7 +53,7 @@ public class SecondFormFragment extends Fragment {
     private boolean validStreetAddress1, validCity, validZip, validInput = false;
 
 
-
+    private String fname;
     private Bundle result = new Bundle();
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -67,15 +67,16 @@ public class SecondFormFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
-
+        Log.d("TAG", "Obtaining first name");
+        /*
         getParentFragmentManager().setFragmentResultListener("sending_first_form_fragment_info", this, new FragmentResultListener() {
                     @Override
                     public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
                         binding.welcomeMessageName.setText(result.getString("First Name"));
                     }
                 });
+
+         */
 
                 binding.nextButton.setOnClickListener(new View.OnClickListener() {
 
@@ -143,7 +144,7 @@ public class SecondFormFragment extends Fragment {
                         if(validInput){
                             getParentFragmentManager().setFragmentResult("sending_second_form_fragment_info", result);
                             NavHostFragment.findNavController(SecondFormFragment.this)
-                                    .navigate(R.id.action_DBR_SecondFormFragment_to_DBR_ThirdFormFragment);
+                                    .navigate(R.id.action_DBR_SecondFormFragment_to_DBR_SummaryFragment);
                         }
                     }
                 });
