c9ab2cf3b01e8eb92cdc24d54e1297c48e0c82dc
Regiane Saccaro <regianemtv@gmail.com>
2021-12-19 20:54:26 -0500

    Code was added throughout the app so when the Login button localized at the app bar is clicked the Login Activty will be launched.

--- app/src/main/java/edu/ncc/nest/nestapp/LoginActivity.java
+++ app/src/main/java/edu/ncc/nest/nestapp/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.ncc.nest.nestapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +12,5 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
     }
-}
+
+    }
